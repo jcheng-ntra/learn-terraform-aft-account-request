6c15f1c3eb4df0d4feecb37e1dcec9ac778aaae2
John Cheng <jcheng@natera.com>
2023-11-29 13:34:59 -0800

    adding sandbox4

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,4 +27,34 @@ module "sandbox" {
 }
 
 
+module "sandbox4" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jcheng+dev4+sandbox4@natera.com"
+    AccountName               = "sandbox4"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "jcheng+dev4+sandbox4@natera.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+
+
 
